c70d292505b6e0249a7fb31be2b1ca4a53c3c6a0
Documenter.jl <documenter@juliadocs.github.io>
2022-02-02 07:48:37 +0000

    build based on 40f6f7b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Ion","category":"page"},{"location":"#Ion","page":"Home","title":"Ion","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Ion]","category":"page"}]
+}
